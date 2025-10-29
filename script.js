a7f597c927223cf529849516cc8f2b1827976b87
Renato Alves de Sousa <renato.alvs1103@gmail.com>
2025-10-29 17:26:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
